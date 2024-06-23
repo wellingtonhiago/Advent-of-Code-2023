b27f28608f8d22d4614713bf945387d4a3bfa665
wellington.silva <wellington.silva@inteligate.com.br>
2024-06-23 10:54:59 -0300

    solução do dia 1, parte 1 e 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,58 @@
 fun main() {
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return solvePart1(input)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return solvePart2(input)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+
+
+
 }
+
+val words: Map<String, Int> = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
+)
+
+fun String.possibleWordsAt(startingAt: Int): List<String> =
+    (3..5).map { len ->
+        substring(startingAt, (startingAt + len).coerceAtMost(length))
+    }
+
+fun calibrationValue(row: String): Int =
+    "${row.first { it.isDigit() }}${row.last { it.isDigit() }}".toInt()
+
+fun solvePart1(input: List<String>): Int =
+    input.sumOf { calibrationValue(it) }
+
+fun solvePart2(input: List<String>): Int =
+    input.sumOf { row ->
+        calibrationValue(
+            row.mapIndexedNotNull { index, c ->
+                if (c.isDigit()) c
+                else
+                    row.possibleWordsAt(index).firstNotNullOfOrNull { candidate ->
+                        words[candidate]
+                    }
+            }.joinToString()
+        )
+    }
